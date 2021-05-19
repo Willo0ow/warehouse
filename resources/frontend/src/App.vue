70b2f05dd574a0b9fa8b7844f5cc6bc66df5a447
Willo0ow <dhinc39@gmail.com>
2021-05-20 01:44:53 +0200

    Vuetify default Home view and App component customized

--- resources/frontend/src/App.vue
+++ resources/frontend/src/App.vue
@@ -6,38 +6,21 @@
       dark
     >
       <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+        <v-icon class="mr-4" large>mdi-warehouse</v-icon>
+      <v-toolbar-title>WarehouseApp</v-toolbar-title>
       </div>
-
       <v-spacer></v-spacer>
-
       <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
+        link
+        to="login"
         text
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+        <span class="mr-2">Login</span>
+        <v-icon>mdi-login-variant</v-icon>
       </v-btn>
     </v-app-bar>
 
-    <v-main style="background: 'background'">
+    <v-main>
       <router-view/>
     </v-main>
   </v-app>
